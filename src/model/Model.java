ebcaeb5734bdfef98c18573c413a1b8396736a16
Yuhua Mai <yuhua.mai@duke.edu>
2013-10-04 13:21:30 -0400

    Add ParseInput Function

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.List;
+
 public abstract class Model {
-    public abstract void parseInput(String userInput);
+    public abstract void updateTrace(String userInput);
+    public abstract List<String> parseInput(String userInput);
 }
