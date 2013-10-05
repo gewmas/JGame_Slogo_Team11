850e6b54b5e5926eaee90ffa0c42bb9e37b355c1
Yuhua Mai <yuhua.mai@duke.edu>
2013-10-04 21:25:27 -0400

    Add FunctionExpression

--- src/model/DefaultModel.java
+++ src/model/DefaultModel.java
@@ -3,12 +3,13 @@ package model;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import model.parser.DefaultParser;
 import model.parser.Parser;
 import model.parser.expression.Expression;
 
 public class DefaultModel extends Model {
-
+    
     public void updateTrace (String userInput) {
         //"sum 5 sum 8 9"
         List<String> commandInput = new ArrayList<String>(Arrays.asList(userInput.split("\\s+")));       
@@ -31,7 +32,8 @@ public class DefaultModel extends Model {
 //        model.updateTrace("fd sum sum 2 sum 1 sum 5 sum 8 9 10");
 //        model.updateTrace("repeat sum 1 2 [ fd sum 1 2 ]");
 //        model.updateTrace("repeat sum 1 2 [ fd sum 1 2 ] fd sum 1 2");
-        model.updateTrace("repeat sum 1 2 [ fd sum 1 2 repeat 3 [ fd sum 1 2 ] ] fd sum 1 2");
+//        model.updateTrace("repeat sum 1 2 [ fd sum 1 2 repeat 3 [ fd sum 1 2 ] ] fd sum 1 2");
+        model.updateTrace("to dash [ :count ] [ repeat :count  [  pu fd 4 pd fd 4 ]  ]   ");
         
     }
 }
