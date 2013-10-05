9dcb270255ee1f0198454e8492508f0f4fccd178
Yuhua Mai <yuhua.mai@duke.edu>
2013-10-04 23:18:45 -0400

    Refactor OneParameterExpression and TwoParameterExpression

--- src/model/parser/expression/ForwardExpression.java
+++ src/model/parser/expression/ForwardExpression.java
@@ -1,34 +1,12 @@
 package model.parser.expression;
 
 import java.util.List;
-import model.parser.DefaultParser;
 
-public class ForwardExpression extends Expression {
-    Expression expression;
-    
-    public ForwardExpression(List<String> cmdList){
-        convert(cmdList);
-    }
-    
-    public void convert(List<String> cmdList) {
-        cmdList.remove(0);
-        
-        try
-        {
-            expression = new NumberExpression(Double.parseDouble(cmdList.get(0)));
-            cmdList.remove(0);
-        }
-        catch(NumberFormatException e)
-        {
-            expression = DefaultParser.parse(cmdList);
-        }
-        
-    }
-    
-    @Override
-    public Expression evaluate () {
-        expression = expression.evaluate();
-        return this;
+
+public class ForwardExpression extends OneParameterExpression {
+
+    public ForwardExpression (List<String> cmdList) {
+        super(cmdList);
     }
 
 }
