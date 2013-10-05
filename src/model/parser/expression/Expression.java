f8c709a615468cd8290a2097e825a493ed5a3b64
fabioberger <fabioberger1991@gmail.com>
2013-10-04 23:49:11 -0400

    fixed issues

--- src/model/parser/expression/Expression.java
+++
@@ -1,9 +0,0 @@
-package model.parser.expression;
-
-import java.util.List;
-
-public abstract class Expression {
-
-    public abstract void convert(List<String> cmdList);
-    public abstract List<Expression> evaluate();
-}
