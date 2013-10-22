9bb52308dd1d1f0b6745ac7f530473cc36cf303a
Adam Sommer <asommer12@gmail.com>
2013-10-22 14:02:02 -0400

    load save

--- src/viewer/toggle/LoadPreferencesButton.java
+++ src/viewer/toggle/LoadPreferencesButton.java
@@ -18,26 +18,30 @@ public class LoadPreferencesButton extends SelectableListButton {
 		super(PREFERENCES, PREFERENCES, BUTTON_TITLE, DIALOG_MESSAGE, controller);
 	}
 	
-	private String[] getAllPreferenceNames() {
-		String[] preferenceNames = new String[this.myController.getAllPreferences().size()];
-		for (Integer i = 0; i < preferenceNames.length; i++) {
-			preferenceNames[i] = i.toString();
-		}
-		return preferenceNames;
+	private int getAllPreferenceNames() {
+//		int[] preferenceNames = new int[this.myController.getAllPreferences().size()];
+//		for (Integer i = 0; i < preferenceNames.length; i++) {
+//			preferenceNames[i] = i;
+//		}
+		return this.myController.getAllPreferences().size();
+//		return preferenceNames.length;
 	}
 	
 	@Override
 	public void buttonPushed() {
 		this.myListModel.clear();
-		String[] preferenceNames = getAllPreferenceNames();
-		for (int i = 0; i < preferenceNames.length; i++) {
-			myListModel.addElement(preferenceNames[i]);
+		int numOptions = getAllPreferenceNames();
+		for (int i = 0; i < numOptions; i++) {
+			myListModel.addElement(String.valueOf(i));
+			System.out.println(i);
 		}
+		super.buttonPushed();
 		if (!myListModel.isEmpty()) {
-			System.out.println(preferenceNames.length);
-			System.out.println(preferenceNames[0]);
-		    this.myController.loadPreferences(Integer.parseInt(preferenceNames[myList.getSelectedIndex()]));
+			System.out.println(numOptions);
+			int selected = myList.getSelectedIndex();
+			if (selected > -1) {
+			    this.myController.loadPreferences(selected);				
+			}
 		}
 	}
-
 }
